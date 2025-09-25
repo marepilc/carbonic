b3ef1b9529b22d17572e74354c22610475f1d611
Marek <user@mp76.pl>
2025-09-25 17:58:45 +0200

    Period class

--- carbonic/__init__.py
+++ carbonic/__init__.py
@@ -7,6 +7,7 @@ A fluent, immutable datetime library built with dataclasses and stdlib zoneinfo.
 from carbonic.core.datetime import DateTime
 from carbonic.core.date import Date
 from carbonic.core.duration import Duration
+from carbonic.core.period import Period
 
 __version__ = "0.1.0"
 
@@ -14,6 +15,7 @@ __all__ = [
     "DateTime",
     "Date",
     "Duration",
+    "Period",
     "now",
     "today",
 ]
